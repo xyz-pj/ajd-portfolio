3cc4f7a5c29ab849feedbe217bb6dcf91ab07f8c
xyz-pj <can8408@gmail.com>
2025-01-17 08:04:07 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   outputDir: './docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/ajd-portfolio/' : '/'
+  publicPath: '/ajd-portfolio/'
 }
